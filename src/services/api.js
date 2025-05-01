846fc5475eb5e6c0d7fad36fc6be026b485b1f9a
Samarth <samarthhegde93@gmail.com>
2025-05-01 12:45:37 +0530

    frontned small api  update

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_BASE_URL = process.env.REACT_APP_API_BASE;
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
