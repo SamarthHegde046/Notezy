18a765fe6d1ad687e352f81d0c7a9e6b7b8cf63c
Samarth <samarthhegde93@gmail.com>
2025-06-13 22:48:21 +0530

    uploadmodal changes

--- src/pages/DepartmentPage.js
+++ src/pages/DepartmentPage.js
@@ -19,7 +19,7 @@ const DepartmentPage = () => {
 
   const departmentMap = {
     computerscience: 'Computer Science',
-    electronics: 'Electronics and Communications',
+    electronicsandcommunications: 'Electronics and Communications',
     aiml: 'AIML',
     informationscience: 'Information Science',
     aids: 'AIDS'
